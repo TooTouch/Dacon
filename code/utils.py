9ecdcd6b7c5afcd90cb7a1b6c50c5e0b035e144e
bllfpc <wogur379@gmail.com>
2019-06-25 00:08:55 +0900

    all

--- code/utils.py
+++
@@ -1,7 +0,0 @@
-from sklearn.metrics import mean_squared_error
-
-def wrmse(y_true, y_pred, AB):
-    return mean_squared_error(y_true, y_pred, sample_weight=AB) ** 0.5
-
-def rmse(y_true, y_pred):
-    return mean_squared_error(y_true, y_pred) ** 0.5
